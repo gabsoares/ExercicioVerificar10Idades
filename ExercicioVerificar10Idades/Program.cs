d4859a8e5473891b767a1d668d6ffb1bdc6d876c
gabsoares <soaresgabriel68@gmail.com>
2024-04-19 22:56:17 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioVerificar10Idades/Program.cs
@@ -0,0 +1,21 @@
+﻿int idade = 0, contador = 0, menorIdade = 0, maiorIdade = 0;
+
+while (contador < 10)
+{
+    Console.Write("Digite uma idade: ");
+    idade = int.Parse(Console.ReadLine());
+    if(idade >= 18)
+    {
+        maiorIdade++;
+    }
+    else
+    {
+        menorIdade++;
+    }
+    contador++;
+}
+
+Console.WriteLine($"O número de pessoas com idade de 18 ou acima: {maiorIdade}");
+Console.WriteLine($"O número de pessoas com menos de 18 anos: {menorIdade}");
+
+
