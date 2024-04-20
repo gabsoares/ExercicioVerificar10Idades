aa4bd91381991338a09c8a262bee246f92dd1612
gabsoares <soaresgabriel68@gmail.com>
2024-04-19 23:03:37 -0300

    Feature para implementar exceção para idade igual ou menor que zero.

--- ExercicioVerificar10Idades/Program.cs
+++ ExercicioVerificar10Idades/Program.cs
@@ -1,21 +1,31 @@
-﻿int idade = 0, contador = 0, menorIdade = 0, maiorIdade = 0;
+﻿int idade = 1, contador = 0, menorIdade = 0, maiorIdade = 0;
 
-while (contador < 10)
+while (contador < 10 && idade > 0)
 {
     Console.Write("Digite uma idade: ");
     idade = int.Parse(Console.ReadLine());
-    if(idade >= 18)
-    {
-        maiorIdade++;
-    }
-    else
-    {
-        menorIdade++;
-    }
+        if (idade >= 18)
+        {
+            maiorIdade++;
+        }
+        else
+        {
+            menorIdade++;
+        }
+
     contador++;
 }
 
-Console.WriteLine($"O número de pessoas com idade de 18 ou acima: {maiorIdade}");
-Console.WriteLine($"O número de pessoas com menos de 18 anos: {menorIdade}");
+if (idade > 0)
+{
+    Console.WriteLine($"O número de pessoas com idade de 18 ou acima: {maiorIdade}");
+    Console.WriteLine($"O número de pessoas com menos de 18 anos: {menorIdade}");
+}
+else
+{
+    Console.WriteLine("Não existe pessoa com 0 anos de idade");
+}
+
+
 
 
