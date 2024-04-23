327f3731f5550221240c834a156070cb6b2d5d40
gabsoares <soaresgabriel68@gmail.com>
2024-04-22 23:48:56 -0300

    Feature - substituição if/else por switch case

--- ExercicioVerificar10Idades/Program.cs
+++ ExercicioVerificar10Idades/Program.cs
@@ -1,30 +1,25 @@
-﻿int idade = 1, contador = 0, menorIdade = 0, maiorIdade = 0;
+﻿int idade = 0, contador = 0, menorIdade = 0, maiorIdade = 0;
 
-while (contador < 10 && idade > 0)
+while (contador < 10)
 {
-    Console.Write("Digite uma idade: ");
+    Console.Write("Digite uma idade válida: ");
     idade = int.Parse(Console.ReadLine());
-        if (idade >= 18)
-        {
+    switch (idade)
+    {
+        case >= 18:
             maiorIdade++;
-        }
-        else
-        {
+            break;
+        default:
             menorIdade++;
-        }
-
+            break;
+    }
     contador++;
 }
 
-if (idade > 0)
-{
-    Console.WriteLine($"O número de pessoas com idade de 18 ou acima: {maiorIdade}");
-    Console.WriteLine($"O número de pessoas com menos de 18 anos: {menorIdade}");
-}
-else
-{
-    Console.WriteLine("Não existe pessoa com 0 anos de idade");
-}
+
+
+Console.WriteLine($"O número de pessoas com idade de 18 ou acima: {maiorIdade}");
+Console.WriteLine($"O número de pessoas com menos de 18 anos: {menorIdade}");
 
 
 
